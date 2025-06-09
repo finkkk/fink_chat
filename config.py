c5152f2eab7a73810a2b6cdc268f9b33daa116eb
finkkk <flx2217183968@gmail.com>
2025-06-09 13:57:50 +0800

    更新配置信息

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
-APP_VERSION = "v1.2.4"
+APP_VERSION = "v1.2.5"
 APP_UPDATED = "2025-06-09"
 
 # 身份管理配置
 GUEST_USERNAME = "游客"
-ADMIN_USERNAMES = ["finkkk", "lite_admin"]
+ADMIN_USERNAMES = ["lite_admin"]
 SUPER_ADMIN_USERNAMES = ["finkkk"]
